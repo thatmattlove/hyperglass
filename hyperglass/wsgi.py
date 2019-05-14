0607fa539ecb13c09d2c4e4eba291dd5a78fa37e
checktheroads <mtt.lov@gmail.com>
2019-05-14 16:39:32 -0700

    fixed HTML/CSS rendering functions in wsgi

--- hyperglass/wsgi.py
+++ hyperglass/wsgi.py
@@ -2,6 +2,7 @@
 import os
 import hyperglass
 from hyperglass.hyperglass import app
+from hyperglass import render
 
 application = app
 
@@ -9,5 +10,6 @@ hyperglass_root = os.path.dirname(hyperglass.__file__)
 static = os.path.join(hyperglass_root, "static")
 
 if __name__ == "__main__":
-    application.renderCSS()
+    hyperglass.render.css.renderTemplate()
+    hyperglass.render.html.renderTemplate()
     application.run(static_folder=static)
