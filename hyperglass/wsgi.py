9010542127338db287d6469a7e2ce0b46c189863
checktheroads <mtt.lov@gmail.com>
2019-05-28 14:14:42 -0700

    gunicorn & wsgi config changes

--- hyperglass/wsgi.py
+++ hyperglass/wsgi.py
@@ -1,17 +1,10 @@
-#!/usr/bin/env python3
 import os
-import hyperglass
-from hyperglass.hyperglass import app
+import hyperglass.hyperglass
 
-application = app
+application = hyperglass.hyperglass.app
 
 hyperglass_root = os.path.dirname(hyperglass.__file__)
 static = os.path.join(hyperglass_root, "static")
 
 if __name__ == "__main__":
-    try:
-        hyperglass.render.css.renderTemplate()
-        hyperglass.render.html.renderTemplate()
-        application.run(static_folder=static)
-    except:
-        raise
+    application.run(static_folder=static)
