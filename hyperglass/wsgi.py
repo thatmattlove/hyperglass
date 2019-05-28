390d70480c30748ceb58bb15d50392a4320ffa44
checktheroads <mtt.lov@gmail.com>
2019-05-28 12:23:43 -0700

    added try/except logic to wsgi params

--- hyperglass/wsgi.py
+++ hyperglass/wsgi.py
@@ -9,6 +9,9 @@ hyperglass_root = os.path.dirname(hyperglass.__file__)
 static = os.path.join(hyperglass_root, "static")
 
 if __name__ == "__main__":
-    hyperglass.render.css.renderTemplate()
-    hyperglass.render.html.renderTemplate()
-    application.run(static_folder=static)
+    try:
+        hyperglass.render.css.renderTemplate()
+        hyperglass.render.html.renderTemplate()
+        application.run(static_folder=static)
+    except:
+        raise
