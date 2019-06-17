34cab77b914ecdd7861ddc0626f0535e076f28a2
checktheroads <mtt.lov@gmail.com>
2019-06-16 17:19:38 -0700

    travis: deps script update 1

--- ci/ci_dependencies.sh
+++ ci/ci_dependencies.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 install_dependencies() {
-  sudo add-apt-repository universe
-  sudo apt-get update -q
-  sudo apt-get install -y redis
+  add-apt-repository universe
+  apt-get update -q
+  apt-get install -y redis
 }
 
 install_dependencies()
