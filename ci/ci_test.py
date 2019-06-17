f20c48fb1c2cf9564176730bce781afa3a6d228b
checktheroads <mtt.lov@gmail.com>
2019-06-16 19:25:51 -0700

    fixed os.path issue

--- ci/ci_test.py
+++ ci/ci_test.py
@@ -13,11 +13,8 @@ def ci_config():
     """Copies test configuration files to usable config files"""
     logger.info("Migrating test config files...")
     config_dir = os.path.join(working_directory, "hyperglass/configuration/")
-    ci_dir = os.path.join(working_directory, "ci/")
-    test_files = glob.iglob(os.path.join(ci_dir, "*.toml"))
+    test_files = glob.iglob(os.path.join(working_directory, "*.toml"))
     logger.debug(config_dir)
-    logger.debug(ci_dir)
-    logger.debug(test_files)
     status = False
     for f in test_files:
         logger.debug(f)
