d9b566c30c75aa2176ef5a1fec0ad6b6a871092d
checktheroads <mtt.lov@gmail.com>
2019-06-18 13:51:37 -0700

    removed unused ci shell script, made ci python script permissions consistent

--- tests/ci_commit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git_commit() {
-  git remote add origin https://${GH_TOKEN}@github.com/checktheroads/hyperglass.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin master
-}
-
-git_commit
