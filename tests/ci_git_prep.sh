337e945f60f7d039240ff43c4fbc39301fb22e71
checktheroads <mtt.lov@gmail.com>
2019-06-18 16:05:52 -0700

    reversed PR validation logic

--- tests/ci_git_prep.sh
+++ tests/ci_git_prep.sh
@@ -9,10 +9,10 @@ git_setup() {
 }
 
 detect_branch() {
-  if [ "$TRAVIS_PULL_REQUEST" = "true" ]; then
-    echo $TRAVIS_PULL_REQUEST_BRANCH
-  else
+  if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     echo $TRAVIS_BRANCH
+  else
+    echo $TRAVIS_PULL_REQUEST_BRANCH
   fi
 }
 
