9b3344cad296b3905ef20bc5309d39538f3d33d9
checktheroads <matt@allroads.io>
2020-01-19 22:06:16 -0700

    add custom error & loading pages

---
+++ ui/pages/_app.js
@@ -0,0 +1,26 @@
+import React from "react";
+import useAxios from "axios-hooks";
+import { HyperglassProvider } from "~/components/HyperglassProvider";
+import PreConfig from "~/components/PreConfig";
+
+const Hyperglass = ({ Component, pageProps }) => {
+    const [{ data, loading, error }, refetch] = useAxios({
+        url: "/config",
+        method: "get"
+    });
+    return (
+        <>
+            {!data ? (
+                <PreConfig loading={loading} error={error} refresh={refetch} />
+            ) : (
+                <HyperglassProvider config={data}>
+                    <Component {...pageProps} />
+                </HyperglassProvider>
+            )}
+        </>
+    );
+};
+
+Hyperglass.displayName = "Hyperglass";
+
+export default Hyperglass;
