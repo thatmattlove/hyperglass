5128c18c7662bd8fb38a8ce154e3c4b1db08d27d
checktheroads <matt@allroads.io>
2020-02-22 00:06:35 -0700

    fix build jobs

--- .tests/dockersetup.sh
+++ .tests/dockersetup.sh
@@ -7,7 +7,10 @@ sleep 2
 echo "Starting UI build"
 poetry run hyperglass build-ui
 echo "UI build completed"
-# echo "Starting redis..."
-# redis-server &
-# echo "Redis started"
-echo ""
+echo "Starting hyperglass..."
+poetry run hyperglass start &> /var/log/hyperglassci.log &
+echo "Started hyperglass"
+echo "Running HTTP test..."
+curl -s -o /dev/null -w "%{http_code}" http://localhost:8001
+echo "Tests ran successfully"
+exit 0
