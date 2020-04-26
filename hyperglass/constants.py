a0ac4dbffef191d0d6c34936fdb31709ec90c2f9
checktheroads <matt@allroads.io>
2020-04-26 09:07:09 -0700

    bump version to 1.0.0-beta30

--- hyperglass/constants.py
+++ hyperglass/constants.py
@@ -4,7 +4,7 @@
 from datetime import datetime
 
 __name__ = "hyperglass"
-__version__ = "1.0.0-beta.29"
+__version__ = "1.0.0-beta.30"
 __author__ = "Matt Love"
 __copyright__ = f"Copyright {datetime.now().year} Matthew Love"
 __license__ = "BSD 3-Clause Clear License"
