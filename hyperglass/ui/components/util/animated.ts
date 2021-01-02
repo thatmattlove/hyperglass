4c9bfe77e063c1546fc2316040ab4c1ea2dda45d
checktheroads <matt@allroads.io>
2021-01-02 12:54:59 -0700

    fix invalid prop forwarding for custom motion components

--- hyperglass/ui/components/util/animated.ts
+++ hyperglass/ui/components/util/animated.ts
@@ -1,8 +1,13 @@
 import { chakra } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
 
-export const AnimatedDiv = motion.custom(chakra.div);
-export const AnimatedForm = motion.custom(chakra.form);
-export const AnimatedH1 = motion.custom(chakra.h1);
-export const AnimatedH3 = motion.custom(chakra.h3);
-export const AnimatedButton = motion.custom(chakra.button);
+/**
+ * Even though this seems to do nothing, this fixes the issue of Chakra Factory forwarding
+ * framer-motion props when it shouldn't.
+ *
+ * @see https://chakra-ui.com/docs/features/chakra-factory
+ */
+const shouldForwardProp = () => true;
+
+export const AnimatedDiv = chakra(motion.div, { shouldForwardProp });
+export const AnimatedForm = chakra(motion.form, { shouldForwardProp });
