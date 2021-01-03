ab47e0e9a0087ccbc3551ccae223df814bc181ca
checktheroads <matt@allroads.io>
2021-01-03 11:10:37 -0700

    test pre-commit hook

--- hyperglass/ui/nextdev.js
+++ hyperglass/ui/nextdev.js
@@ -8,7 +8,6 @@ const config = require(String(configFile));
 
 const { NODE_ENV: env, _HYPERGLASS_URL_: envUrl } = config;
 
-// testing
 const devProxy = {
   '/api/query/': { target: envUrl + 'api/query/', pathRewrite: { '^/api/query/': '' } },
   '/images': { target: envUrl + 'images', pathRewrite: { '^/images': '' } },
