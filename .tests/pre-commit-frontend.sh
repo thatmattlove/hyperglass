0da2776f53721d982b7666170acf0ea55ecabd6b
checktheroads <matt@allroads.io>
2021-01-03 11:16:03 -0700

    test pre-commit hook

--- .tests/pre-commit-frontend.sh
+++ .tests/pre-commit-frontend.sh
@@ -2,11 +2,14 @@
 
 UI_DIR="./hyperglass/ui"
 
-if git diff --cached --name-only | grep --quiet $UI_DIR
-then
-    cd $UI_DIR
-    node_modules/.bin/tsc
-else
-    echo "No frontend files have changed, skipping pre-commit check..."
-    exit 0
-fi
+cd $UI_DIR
+node_modules/.bin/tsc
+
+# if git diff --cached --name-only | grep --quiet $UI_DIR
+# then
+#     cd $UI_DIR
+#     node_modules/.bin/tsc
+# else
+#     echo "No frontend files have changed, skipping pre-commit check..."
+#     exit 0
+# fi
