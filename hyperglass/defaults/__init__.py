37a8e40bfecae406857439443841142ce8dc7f94
thatmattlove <matt@hyperglass.dev>
2021-09-16 17:13:15 -0700

    Move defaults into dedicated module

---
+++ hyperglass/defaults/__init__.py
@@ -0,0 +1,11 @@
+"""Default or built-in hyperglass data."""
+
+# Local
+from ._strings import CREDIT, DEFAULT_HELP, DEFAULT_TERMS, DEFAULT_DETAILS
+
+__all__ = (
+    "CREDIT",
+    "DEFAULT_TERMS",
+    "DEFAULT_DETAILS",
+    "DEFAULT_HELP",
+)
