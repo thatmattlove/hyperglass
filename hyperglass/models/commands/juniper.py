ce13b764f2489fdd19c3b9cd685f1c7e4f07865e
Remi Frenay <rf@worldstream.com>
2022-05-12 16:03:10 +0200

    Bugfix: allow updating Arista an Juniper commands

--- hyperglass/models/commands/juniper.py
+++ hyperglass/models/commands/juniper.py
@@ -10,9 +10,9 @@ from .common import CommandSet, CommandGroup
 class _IPv4(CommandSet):
     """Validation model for non-default dual afi commands."""
 
-    bgp_route: StrictStr = 'show route protocol bgp table inet.0 {target} best detail | except Label | except Label | except "Next hop type" | except Task | except Address | except "Session Id" | except State | except "Next-hop reference" | except destinations | except "Announcement bits"'
-    bgp_aspath: StrictStr = 'show route protocol bgp table inet.0 aspath-regex "{target}"'
-    bgp_community: StrictStr = "show route protocol bgp table inet.0 community {target}"
+    bgp_route: StrictStr = "show route protocol bgp table inet.0 {target} best detail | display xml"
+    bgp_aspath: StrictStr = 'show route protocol bgp table inet.0 aspath-regex "{target}" detail | display xml'
+    bgp_community: StrictStr = "show route protocol bgp table inet.0 community {target} detail | display xml"
     ping: StrictStr = "ping inet {target} count 5 source {source}"
     traceroute: StrictStr = "traceroute inet {target} wait 1 source {source}"
 
@@ -20,9 +20,9 @@ class _IPv4(CommandSet):
 class _IPv6(CommandSet):
     """Validation model for non-default ipv4 commands."""
 
-    bgp_route: StrictStr = 'show route protocol bgp table inet6.0 {target} best detail | except Label | except Label | except "Next hop type" | except Task | except Address | except "Session Id" | except State | except "Next-hop reference" | except destinations | except "Announcement bits"'
-    bgp_aspath: StrictStr = 'show route protocol bgp table inet6.0 aspath-regex "{target}"'
-    bgp_community: StrictStr = "show route protocol bgp table inet6.0 community {target}"
+    bgp_route: StrictStr = "show route protocol bgp table inet6.0 {target} best detail | display xml"
+    bgp_aspath: StrictStr = 'show route protocol bgp table inet6.0 aspath-regex "{target}" detail | display xml'
+    bgp_community: StrictStr = "show route protocol bgp table inet6.0 community {target} detail | display xml"
     ping: StrictStr = "ping inet6 {target} count 5 source {source}"
     traceroute: StrictStr = "traceroute inet6 {target} wait 2 source {source}"
 
@@ -30,9 +30,9 @@ class _IPv6(CommandSet):
 class _VPNIPv4(CommandSet):
     """Validation model for non-default ipv6 commands."""
 
-    bgp_route: StrictStr = 'show route protocol bgp table {vrf}.inet.0 {target} best detail | except Label | except Label | except "Next hop type" | except Task | except Address | except "Session Id" | except State | except "Next-hop reference" | except destinations | except "Announcement bits"'
-    bgp_aspath: StrictStr = 'show route protocol bgp table {vrf}.inet.0 aspath-regex "{target}"'
-    bgp_community: StrictStr = "show route protocol bgp table {vrf}.inet.0 community {target}"
+    bgp_route: StrictStr = "show route protocol bgp table {vrf}.inet.0 {target} best detail | display xml"
+    bgp_aspath: StrictStr = 'show route protocol bgp table {vrf}.inet.0 aspath-regex "{target}" detail | display xml'
+    bgp_community: StrictStr = "show route protocol bgp table {vrf}.inet.0 community {target} detail | display xml"
     ping: StrictStr = "ping inet routing-instance {vrf} {target} count 5 source {source}"
     traceroute: StrictStr = "traceroute inet routing-instance {vrf} {target} wait 1 source {source}"
 
@@ -40,43 +40,11 @@ class _VPNIPv4(CommandSet):
 class _VPNIPv6(CommandSet):
     """Validation model for non-default ipv6 commands."""
 
-    bgp_route: StrictStr = 'show route protocol bgp table {vrf}.inet6.0 {target} best detail | except Label | except Label | except "Next hop type" | except Task | except Address | except "Session Id" | except State | except "Next-hop reference" | except destinations | except "Announcement bits"'
-    bgp_aspath: StrictStr = 'show route protocol bgp table {vrf}.inet6.0 aspath-regex "{target}"'
-    bgp_community: StrictStr = "show route protocol bgp table {vrf}.inet6.0 community {target}"
+    bgp_route: StrictStr = "show route protocol bgp table {vrf}.inet6.0 {target} best detail | display xml"
+    bgp_aspath: StrictStr = 'show route protocol bgp table {vrf}.inet6.0 aspath-regex "{target}" detail | display xml'
+    bgp_community: StrictStr = "show route protocol bgp table {vrf}.inet6.0 community {target} detail | display xml"
     ping: StrictStr = "ping inet6 routing-instance {vrf} {target} count 5 source {source}"
     traceroute: StrictStr = "traceroute inet6 routing-instance {vrf} {target} wait 2 source {source}"
-
-
-_structured = CommandGroup(
-    ipv4_default=CommandSet(
-        bgp_route="show route protocol bgp table inet.0 {target} best detail | display xml",
-        bgp_aspath='show route protocol bgp table inet.0 aspath-regex "{target}" detail | display xml',
-        bgp_community="show route protocol bgp table inet.0 community {target} detail | display xml",
-        ping="ping inet {target} count 5 source {source}",
-        traceroute="traceroute inet {target} wait 1 source {source}",
-    ),
-    ipv6_default=CommandSet(
-        bgp_route="show route protocol bgp table inet6.0 {target} best detail | display xml",
-        bgp_aspath='show route protocol bgp table inet6.0 aspath-regex "{target}" detail | display xml',
-        bgp_community="show route protocol bgp table inet6.0 community {target} detail | display xml",
-        ping="ping inet6 {target} count 5 source {source}",
-        traceroute="traceroute inet6 {target} wait 2 source {source}",
-    ),
-    ipv4_vpn=CommandSet(
-        bgp_route="show route protocol bgp table {vrf}.inet.0 {target} best detail | display xml",
-        bgp_aspath='show route protocol bgp table {vrf}.inet.0 aspath-regex "{target}" detail | display xml',
-        bgp_community="show route protocol bgp table {vrf}.inet.0 community {target} detail | display xml",
-        ping="ping inet routing-instance {vrf} {target} count 5 source {source}",
-        traceroute="traceroute inet routing-instance {vrf} {target} wait 1 source {source}",
-    ),
-    ipv6_vpn=CommandSet(
-        bgp_route="show route protocol bgp table {vrf}.inet6.0 {target} best detail | display xml",
-        bgp_aspath='show route protocol bgp table {vrf}.inet6.0 aspath-regex "{target}" detail | display xml',
-        bgp_community="show route protocol bgp table {vrf}.inet6.0 community {target} detail | display xml",
-        ping="ping inet6 routing-instance {vrf} {target} count 5 source {source}",
-        traceroute="traceroute inet6 routing-instance {vrf} {target} wait 2 source {source}",
-    ),
-)
 
 
 class JuniperCommands(CommandGroup):
@@ -88,6 +56,10 @@ class JuniperCommands(CommandGroup):
     ipv6_vpn: _VPNIPv6 = _VPNIPv6()
 
     def __init__(self, **kwargs):
-        """Initialize command group, ensure structured fields are not overridden."""
         super().__init__(**kwargs)
-        self.structured = _structured
+        self.structured = CommandGroup(
+            ipv4_default=self.ipv4_default,
+            ipv6_default=self.ipv6_default,
+            ipv4_vpn=self.ipv4_vpn,
+            ipv6_vpn=self.ipv6_vpn,
+        )
