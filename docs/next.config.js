bfe3efbc70134fc90ce790732947a78e4938176b
thatmattlove <matt@hyperglass.dev>
2022-12-19 15:44:48 -0500

    add experimental edge to next config

--- docs/next.config.js
+++ docs/next.config.js
@@ -3,4 +3,8 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.tsx',
 });
 
-module.exports = withNextra();
+module.exports = withNextra({
+  experimental: {
+    runtime: 'experimental-edge',
+  },
+});
