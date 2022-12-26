82e526729269c9d7c6f46ca7d5a9e47b2765b8fb
thatmattlove <matt@hyperglass.dev>
2022-12-26 13:03:10 -0500

    improve docs

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -1,5 +1,7 @@
 import styles from './global.module.css';
-import { DocsThemeConfig } from 'nextra-theme-docs';
+import { useRouter } from 'next/router';
+import { useConfig, type DocsThemeConfig } from 'nextra-theme-docs';
+import faviconFormats from './favicon-formats';
 
 const NO_INDEX_FOLLOW = process.env.CF_PAGES_BRANCH !== 'main';
 
@@ -68,10 +70,61 @@ const config: DocsThemeConfig = {
       </svg>
     </span>
   ),
+
+  useNextSeoProps: () => {
+    const { asPath } = useRouter();
+    const { frontMatter, title } = useConfig();
+    return {
+      titleTemplate: '%s | hyperglass',
+      title: frontMatter.title || title,
+      openGraph: {
+        type: 'website',
+        url: `https://hyperglass.dev${asPath}`,
+        title: frontMatter.title || title,
+        description: frontMatter.description || 'hyperglass Documentation',
+        images: [
+          {
+            url: 'https://hyperglass.dev/opengraph.jpg',
+            width: 1200,
+            height: 630,
+            alt: 'hyperglass',
+          },
+        ],
+      },
+      twitter: { handle: '@thatmattlove', site: '@thatmattlove', cardType: 'summary_large_image' },
+      noindex: NO_INDEX_FOLLOW,
+      nofollow: NO_INDEX_FOLLOW,
+      additionalLinkTags: faviconFormats.map(fmt => {
+        const { image_format, dimensions, prefix, rel } = fmt;
+        const [w, h] = dimensions;
+        const href = `/img/${prefix}-${w}x${h}.${image_format}`;
+        return { rel: rel ?? '', href, type: `image/${image_format}` };
+      }),
+    };
+  },
+  banner: {
+    dismissible: true,
+    text: '🎉 hyperglass 2.0 is here!',
+  },
+  feedback: { content: null },
+  footer: { text: `© ${new Date().getFullYear()} hyperglass` },
+  chat: {
+    link: 'https://netdev.chat/',
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 448 512"
+        width="24"
+        height="24"
+        fill="currentColor"
+      >
+        <path d="M94.12 315.1c0 25.9-21.16 47.06-47.06 47.06S0 341 0 315.1c0-25.9 21.16-47.06 47.06-47.06h47.06v47.06zm23.72 0c0-25.9 21.16-47.06 47.06-47.06s47.06 21.16 47.06 47.06v117.84c0 25.9-21.16 47.06-47.06 47.06s-47.06-21.16-47.06-47.06V315.1zm47.06-188.98c-25.9 0-47.06-21.16-47.06-47.06S139 32 164.9 32s47.06 21.16 47.06 47.06v47.06H164.9zm0 23.72c25.9 0 47.06 21.16 47.06 47.06s-21.16 47.06-47.06 47.06H47.06C21.16 243.96 0 222.8 0 196.9s21.16-47.06 47.06-47.06H164.9zm188.98 47.06c0-25.9 21.16-47.06 47.06-47.06 25.9 0 47.06 21.16 47.06 47.06s-21.16 47.06-47.06 47.06h-47.06V196.9zm-23.72 0c0 25.9-21.16 47.06-47.06 47.06-25.9 0-47.06-21.16-47.06-47.06V79.06c0-25.9 21.16-47.06 47.06-47.06 25.9 0 47.06 21.16 47.06 47.06V196.9zM283.1 385.88c25.9 0 47.06 21.16 47.06 47.06 0 25.9-21.16 47.06-47.06 47.06-25.9 0-47.06-21.16-47.06-47.06v-47.06h47.06zm0-23.72c-25.9 0-47.06-21.16-47.06-47.06 0-25.9 21.16-47.06 47.06-47.06h117.84c25.9 0 47.06 21.16 47.06 47.06 0 25.9-21.16 47.06-47.06 47.06H283.1z" />
+      </svg>
+    ),
+  },
   project: {
     link: 'https://github.com/thatmattlove/hyperglass',
   },
-  useNextSeoProps: () => ({ noindex: NO_INDEX_FOLLOW, nofollow: NO_INDEX_FOLLOW }),
 };
 
 export default config;
