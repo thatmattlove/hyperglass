ff0bed6257f442c4febad7a069d555a1738148bd
Carlos Santos <125412989+CarlosSuporteISP@users.noreply.github.com>
2025-09-13 00:15:15 -0300

    Update __init__.py

--- hyperglass/plugins/_builtin/__init__.py
+++ hyperglass/plugins/_builtin/__init__.py
@@ -5,14 +5,20 @@ from .bgp_route_frr import BGPRoutePluginFrr
 from .remove_command import RemoveCommand
 from .bgp_route_arista import BGPRoutePluginArista
 from .bgp_route_huawei import BGPRoutePluginHuawei
+from .bgp_routestr_huawei import BGPSTRRoutePluginHuawei
 from .bgp_route_juniper import BGPRoutePluginJuniper
 from .mikrotik_garbage_output import MikrotikGarbageOutput
+from .bgp_routestr_mikrotik import BGPSTRRoutePluginMikrotik
+from .mikrotik_normalize_input import MikrotikTargetNormalizerInput
 
 __all__ = (
     "BGPRoutePluginArista",
     "BGPRoutePluginFrr",
     "BGPRoutePluginJuniper",
     "BGPRoutePluginHuawei",
+    "BGPSTRRoutePluginHuawei",
     "MikrotikGarbageOutput",
+    "BGPSTRRoutePluginMikrotik",
+    "MikrotikTargetNormalizerInput",
     "RemoveCommand",
 )
