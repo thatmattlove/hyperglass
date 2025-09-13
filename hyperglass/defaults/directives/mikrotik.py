ab514629f77a9b97a249c73b8649a65b3e1e2831
Carlos Santos <125412989+CarlosSuporteISP@users.noreply.github.com>
2025-09-13 00:09:06 -0300

    Update mikrotik.py

--- hyperglass/defaults/directives/mikrotik.py
+++ hyperglass/defaults/directives/mikrotik.py
@@ -15,6 +15,9 @@ __all__ = (
     "Mikrotik_BGPRoute",
     "Mikrotik_Ping",
     "Mikrotik_Traceroute",
+    "MikrotikBGPRouteTable",
+    "MikrotikBGPASPathTable",
+    "MikrotikBGPCommunityTable",
 )
 
 NAME = "Mikrotik"
@@ -26,16 +29,20 @@ Mikrotik_BGPRoute = BuiltinDirective(
     rules=[
         RuleWithIPv4(
             condition="0.0.0.0/0",
+            ge="8",
+            le="32",
             action="permit",
-            command="ip route print where {target} in dst-address",
+            command="routing route print detail without-paging where {target} in dst-address bgp and dst-address !=0.0.0.0/0",
         ),
         RuleWithIPv6(
             condition="::/0",
             action="permit",
-            command="ipv6 route print where {target} in dst-address",
+            command="routing route print detail without-paging where {target} in dst-address bgp and dst-address !=::/0",
         ),
     ],
     field=Text(description="IP Address, Prefix, or Hostname"),
+    plugins=["mikrotik_normalize_input", "mikrotik_garbage_output", "bgp_routestr_mikrotik"],
+    table_output="__hyperglass_mikrotik_bgp_route_table__",
     platforms=PLATFORMS,
 )
 
@@ -53,6 +60,8 @@ Mikrotik_BGPASPath = BuiltinDirective(
         )
     ],
     field=Text(description="AS Path Regular Expression"),
+    plugins=["mikrotik_normalize_input","mikrotik_garbage_output", "bgp_routestr_mikrotik"],
+    table_output="__hyperglass_mikrotik_bgp_aspath_table__",
     platforms=PLATFORMS,
 )
 
@@ -70,6 +79,8 @@ Mikrotik_BGPCommunity = BuiltinDirective(
         )
     ],
     field=Text(description="BGP Community String"),
+    plugins=["mikrotik_normalize_input","mikrotik_garbage_output", "bgp_routestr_mikrotik"],
+    table_output="__hyperglass_mikrotik_bgp_community_table__",
     platforms=PLATFORMS,
 )
 
@@ -108,5 +119,166 @@ Mikrotik_Traceroute = BuiltinDirective(
         ),
     ],
     field=Text(description="IP Address, Prefix, or Hostname"),
+    platforms=PLATFORMS,
+)
+
+# Table Output Directives
+
+MikrotikBGPRouteTable = BuiltinDirective(
+    id="__hyperglass_mikrotik_bgp_route_table__",
+    name="BGP Route",
+    rules=[
+        # Regra DENY RFC 6598
+        RuleWithIPv4(
+            condition="100.64.0.0/10",
+            ge="10",
+            le="32",
+            action="deny",
+            command="",
+        ),
+        # Regra DENY RFC 1918 classe A
+        RuleWithIPv4(
+            condition="10.0.0.0/8",
+            ge="8",
+            le="32",
+            action="deny",
+            command="",
+        ),
+        # Regra DENY RFC 1918 classe B
+        RuleWithIPv4(
+            condition="172.16.0.0/12",
+            ge="12",
+            le="32",
+            action="deny",
+            command="",
+        ),
+        # Regra DENY RFC 1918 classe C
+        RuleWithIPv4(
+            condition="192.168.0.0/16",
+            ge="16",
+            le="32",
+            action="deny",
+            command="",
+        ),
+        # Regra DENY LO
+        RuleWithIPv4(
+            condition="127.0.0.0/8",
+            ge="8",
+            le="32",
+            action="deny",
+            command="",
+        ),
+        # Regra DENY ASN PREFIXO
+        RuleWithIPv4(
+            condition="${ASN-IPv4}",
+            ge="${mask}",
+            le="32",
+            action="deny",
+            command="",
+        ),
+        # Regra DENY DEFAULT
+        RuleWithIPv4(
+            condition="0.0.0.0/8",
+            ge="8",
+            le="32",
+            action="deny",
+            command="",
+        ),
+        RuleWithIPv4(
+            condition="0.0.0.0/0",
+            ge="8",
+            le="32",
+            action="permit",
+            command="routing route print detail without-paging where {target} in dst-address bgp and dst-address !=0.0.0.0/0",
+        ),
+        # REGRA DENY SITE LOCAL DEPRECIADO RFC 3879
+        RuleWithIPv6(
+            condition="fec0::/10",
+            ge="10",
+            le="128",
+            action="deny",
+            command="",
+        ),
+        # REGRA DENY ULA RFC 4193
+        RuleWithIPv6(
+            condition="fc00::/7",
+            ge="7",
+            le="128",
+            action="deny",
+            command="",
+        ),
+        # REGRA DENY LINK LOCAL RFC 4291
+        RuleWithIPv6(
+            condition="fe80::/10",
+            ge="10",
+            le="128",
+            action="deny",
+            command="",
+        ),
+        # REGRA DENY Unspecified RFC 4291
+        RuleWithIPv6(
+            condition="::/128",
+            ge="128",
+            le="128",
+            action="deny",
+            command="",
+        ),
+        # REGRA DENY LO RFC 4291
+        RuleWithIPv6(
+            condition="::1/128",
+            ge="128",
+            le="128",
+            action="deny",
+            command="",
+        ),
+        # REGRA DENY ASN PREFIXO
+        RuleWithIPv6(
+            condition="${ASN-IPv6}",
+            ge="${mask}",
+            le="128",
+            action="deny",
+            command="",
+        ),
+        RuleWithIPv6(
+            condition="::/0",
+            action="permit",
+            command="routing route print detail without-paging where {target} in dst-address bgp and dst-address !=::/0",
+        ),
+    ],
+    field=Text(description="IP Address, Prefix, or Hostname"),
+    platforms=PLATFORMS,
+)
+
+MikrotikBGPASPathTable = BuiltinDirective(
+    id="__hyperglass_mikrotik_bgp_aspath_table__",
+    name="BGP AS Path",
+    rules=[
+        RuleWithPattern(
+            condition="*",
+            action="permit",
+            commands=[
+                "routing route print detail without-paging where bgp-as-path~{target}",
+                "routing route print detail without-paging where bgp-as-path~{target}",
+            ],
+        )
+    ],
+    field=Text(description="AS Path Regular Expression"),
+    platforms=PLATFORMS,
+)
+
+MikrotikBGPCommunityTable = BuiltinDirective(
+    id="__hyperglass_mikrotik_bgp_community_table__",
+    name="BGP Community",
+    rules=[
+        RuleWithPattern(
+            condition="*",
+            action="permit",
+            commands=[
+                "routing route print detail without-paging where bgp-communities~{target}",
+                "routing route print detail without-paging where bgp-communities~{target}",
+            ],
+        )
+    ],
+    field=Text(description="BGP Community String"),
     platforms=PLATFORMS,
 )
